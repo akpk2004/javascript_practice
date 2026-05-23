c51339f86a8774b07508c478c637609a9cc6ff6a
akpk2004 <136348204+akpk2004@users.noreply.github.com>
2026-05-23 15:00:50 +0530

    Add files via upload

---
+++ 1.variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+//Var is not used because of the scope of the variable 
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 2.datatypes.js
@@ -0,0 +1,20 @@
+console.log("Anubhav")
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; //undefined
+//Primitive data types have been mentioned below-->
+//number has a range of 2 to the power of 53
+//bigint
+//string--> ""
+//boolean-->true or false
+//null-->it is a standalone value in javascript. It is an defined as an object
+//null is the representation of an empty value
+//undefined--> not defined value
+//symbol => it is a unique identifier in javascript
+
+//object
+
+console.log(typeof "Anubhav")
+console.log(typeof null)
+console.log(typeof undefined)
